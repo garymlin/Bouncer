19854e763146f64d2c828b0059ebfe9e066e7aa7
John Hui <jwongsquared@gmail.com>
2016-02-06 10:24:06 -0500

    tweeeeekz to fbchecking

--- static/js/fbCheckin.js
+++ static/js/fbCheckin.js
@@ -20,43 +20,28 @@ window.fbAsyncInit = function() {
     if (response.status === 'connected') {
         userID = response.authResponse.userID;
         
-        $(checkinbutton).click(function() {
-            console.log("what the fuck");
-            console.log(userID);
-            // action= "http://localhost:5000/api/checkin/" method ="post"
-            console.log($(formInput).val());
+        FB.api(window.location.pathname.slice(window.location.pathname.lastIndexOf('/')) + "/admins",
+          function(response) {
+            for(var i = 0; i < response.data.length; i++) {
+              if(response.data[i].id == userID) {
+                  window.location.href = '/checker/' + userID + '/events' + window.location.pathname.slice(window.location.pathname.lastIndexOf('/'));
+              }
+            }
+          }
+          $(checkinbutton).click(function() {
             $.ajax({
                 url:'/api/checkin/'+userID,
                 type:'post',
                 data:$(formInput).val(),
                 sucess:function() {
-                  console.log("this is perfect");
-                  var pathh = window.location.pathname;
-                  window.location.href = window.location.origin + '/checkin/' + userID + '/events/' + pathh.slice(pathh.lastIndexOf('/'));
-                }
-            });
-          //   $(formInput).submit(function(event) {
-          //     console.log(event);
-              
-          // });
-          
-
+                  window.location.href = window.location.origin + '/checker/' + userID + '/events' + window.location.pathname.slice(window.location.pathname.lastIndexOf('/'));
+            }
+          });
+          );
         });
-
-        
     } else {
         	// go back to login page
         	window.location.href = window.location.origin;
   	}
   });
 };
-
-// function checkIn(key) {
-// 	$.ajax({url: "/check-in/"+userID+"/"+key.toLowerCase(), success: function(result){
-//         if(result === '200 OK') {
-//         	// validation succeeded
-//         } else {
-//         	// validation fucked
-//         }
-//     }});
-// }
