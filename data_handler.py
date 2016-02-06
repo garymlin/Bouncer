7ab29a788ca743f4516517d5559ad3c596725dab
Emily Pakulski <enp2111@columbia.edu>
2016-02-06 06:35:31 -0500

    Fixed bug in writing second stage of check in.

--- data_handler.py
+++ data_handler.py
@@ -1,5 +1,6 @@
 import requests
 import redis
+import sys
 
 RANDOM_WORD_COUNT = 4
 EXPIRATION_TIME_SECONDS = 60
@@ -11,9 +12,13 @@ PEOPLE_PREFIX = 'people:' # information on users from Facebook
 LINEAGE_PREFIX = 'parent:' # who checked who in
 
 db = redis.Redis()
+try:
+    db.client_list()
+except redis.ConnectionError:
+    print 'Redis not running.'
+    sys.exit(0)
 
 def generate_key():
-    print 'hi'
     l = []
 
     for i in range(0, RANDOM_WORD_COUNT):
@@ -26,7 +31,6 @@ def generate_key():
 
 def set_key(uid):
     unique_str = generate_key()
-    print unique_str
     db.setex(KEYS_PREFIX + unique_str, uid, EXPIRATION_TIME_SECONDS)
     return unique_str
 
@@ -34,7 +38,7 @@ def verify_key(key):
     return db.exists(KEYS_PREFIX + key)
 
 def confirm_registration(key, uid):
-    parent_uid = db.get(KEYS_PREFIX + key_str) 
+    parent_uid = db.get(KEYS_PREFIX + key) 
     db.delete(KEYS_PREFIX + key)
     db.set(LINEAGE_PREFIX + uid, parent_uid)
 
